5e89bff015f3acc53181e47b125ad6ec8703c81a
Danz2021 <omerdan03@gmail.com>
2022-06-20 20:37:53 +0300

    comment detector for testing FPS

--- pet_detector.py
+++ pet_detector.py
@@ -15,17 +15,17 @@ def main():
         if not ret:
             break
 
-        class_ids, confs, bboxes = detector.detect(frame, conf_threshold=0.5)
-        if len(class_ids) != 0:  # Prevents from assessing when no objected was detected
-            class_names = [detector.class_names[class_id - 1] for class_id in class_ids.flatten()]
-
-            for i, class_id in enumerate(class_names):\
-
-                if class_id == 'dog':
-                    x, y, w, h = bboxes[i]
-                    cv2.putText(frame, class_names[i], (x, y - 15),
-                                cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        # class_ids, confs, bboxes = detector.detect(frame, conf_threshold=0.5)
+        # if len(class_ids) != 0:  # Prevents from assessing when no objected was detected
+        #     class_names = [detector.class_names[class_id - 1] for class_id in class_ids.flatten()]
+        #
+        #     for i, class_id in enumerate(class_names):\
+        #
+        #         if class_id == 'dog':
+        #             x, y, w, h = bboxes[i]
+        #             cv2.putText(frame, class_names[i], (x, y - 15),
+        #                         cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        #             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
         end_time = time.time()
         fps = np.round(1 / (end_time - start_time), 2)
